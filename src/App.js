83b7466a200b90ae3b08f89367bef997ab327b95
xuhaiqiang <xuhaiqiang@xiaoheiban.cn>
2020-06-30 13:49:05 +0800

    lauout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import { HashRouter } from 'react-router-dom';
+import { renderRoutes } from 'react-router-config'
+import { Provider } from 'react-redux'
 import { IconStyle } from './assets/iconfont/iconfont';
 import { GlobalStyle } from './style';
+import routes from './routes';
+import store from './store/index'
 
-function App () {
+function App() {
   return (
-    <div className="App">
-      <GlobalStyle></GlobalStyle>
-      <IconStyle></IconStyle>
-      <i className="iconfont">&#xe62b;</i>
-    </div>
+    <Provider store={store}>
+      <HashRouter>
+        <GlobalStyle></GlobalStyle>
+        <IconStyle></IconStyle>
+        {renderRoutes(routes)}
+      </HashRouter>
+    </Provider>
   );
 }
 
