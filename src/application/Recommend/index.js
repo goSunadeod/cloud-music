fee7ce60607f85bc479c953735d5cec6d863e2f1
xuhaiqiang <xuhaiqiang@xiaoheiban.cn>
2020-07-01 19:30:30 +0800

    首页大致布局

--- src/application/Recommend/index.js
+++ src/application/Recommend/index.js
@@ -1,5 +1,7 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
+import { forceCheck } from 'react-lazyload';
+import Loading from '../../baseUI/Loading/index';
 import Slider from '../../components/Slider';
 import * as actionTypes from './store/actionCreators';
 import RecommendList from '../../components/List';
@@ -7,13 +9,17 @@ import Scroll from '../../baseUI/Scroll';
 import { Content } from './style';
 
 function Recommend(props) {
-  const { bannerList, recommendList } = props;
+  const { bannerList, recommendList, enterLoading } = props;
 
   const { getBannerDataDispatch, getRecommendListDataDispatch } = props;
 
   useEffect(() => {
-    getBannerDataDispatch();
-    getRecommendListDataDispatch();
+    if (!bannerList.size) {
+      getBannerDataDispatch();
+    }
+    if (!recommendList.size) {
+      getRecommendListDataDispatch();
+    }
     //eslint-disable-next-line
   }, []);
 
@@ -22,12 +28,13 @@ function Recommend(props) {
 
   return (
     <Content>
-      <Scroll>
+      <Scroll className="list" onScroll={forceCheck}>
         <div>
           <Slider bannerList={bannerListJS}></Slider>
           <RecommendList recommendList={recommendListJS}></RecommendList>
         </div>
       </Scroll>
+      {enterLoading ? <Loading></Loading> : null}
     </Content>
   );
 }
@@ -38,6 +45,7 @@ const mapStateToProps = (state) => ({
   // 不然每次 diff 比对 props 的时候都是不一样的引用，还是导致不必要的重渲染，属于滥用 immutable
   bannerList: state.getIn(['recommend', 'bannerList']),
   recommendList: state.getIn(['recommend', 'recommendList']),
+  enterLoading: state.getIn(['recommend', 'enterLoading'])
 });
 // 映射 dispatch 到 props 上
 const mapDispatchToProps = (dispatch) => {
