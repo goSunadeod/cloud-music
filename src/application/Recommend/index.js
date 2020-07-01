ff7b6a3471132b71ce1bc4a60163f2461f377f4d
xuhaiqiang <xuhaiqiang@xiaoheiban.cn>
2020-07-01 18:53:11 +0800

    api对接

--- src/application/Recommend/index.js
+++ src/application/Recommend/index.js
@@ -1,9 +1,55 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import Slider from '../../components/Slider';
+import * as actionTypes from './store/actionCreators';
+import RecommendList from '../../components/List';
+import Scroll from '../../baseUI/Scroll';
+import { Content } from './style';
 
-function Recommend (props) {
+function Recommend(props) {
+  const { bannerList, recommendList } = props;
+
+  const { getBannerDataDispatch, getRecommendListDataDispatch } = props;
+
+  useEffect(() => {
+    getBannerDataDispatch();
+    getRecommendListDataDispatch();
+    //eslint-disable-next-line
+  }, []);
+
+  const bannerListJS = bannerList ? bannerList.toJS() : [];
+  const recommendListJS = recommendList ? recommendList.toJS() : [];
+
   return (
-    <div>Recommend</div>
-  )
+    <Content>
+      <Scroll>
+        <div>
+          <Slider bannerList={bannerListJS}></Slider>
+          <RecommendList recommendList={recommendListJS}></RecommendList>
+        </div>
+      </Scroll>
+    </Content>
+  );
 }
 
-export default React.memo (Recommend);
+// 映射 Redux 全局的 state 到组件的 props 上
+const mapStateToProps = (state) => ({
+  // 不要在这里将数据 toJS
+  // 不然每次 diff 比对 props 的时候都是不一样的引用，还是导致不必要的重渲染，属于滥用 immutable
+  bannerList: state.getIn(['recommend', 'bannerList']),
+  recommendList: state.getIn(['recommend', 'recommendList']),
+});
+// 映射 dispatch 到 props 上
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getBannerDataDispatch() {
+      dispatch(actionTypes.getBannerList());
+    },
+    getRecommendListDataDispatch() {
+      dispatch(actionTypes.getRecommendList());
+    },
+  }
+};
+
+// 将 ui 组件包装成容器组件
+export default connect(mapStateToProps, mapDispatchToProps)(React.memo(Recommend));
